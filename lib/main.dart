5c1b6e780c214ee1effe8c3c7170b41ee349bc5e
abassey <anemmeabasi1@gmail.com>
2022-02-25 12:54:20 -0500

    added core vault box and gridview
    add button logic working for ontap within gridview, but adding new element logic not working currently - still to figure out

--- lib/main.dart
+++ lib/main.dart
@@ -15,80 +15,124 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Vocabify'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Vocabify"),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.blue,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.lock),
+              label: 'Bank',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.extension),
+              label: 'Game',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add_box),
+              label: 'Add Word',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.group),
+              label: 'Friends',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Account',
+            ),
+          ],
+          selectedItemColor: Colors.white,
+        ),
+        body: const HomeScreen(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
+List<Widget> gridChild = [
+  Stack(
+    children: [
+      Positioned.fill(
+        child: Container(
+          margin: const EdgeInsets.all(10.0),
+          width: 30.0,
+          height: 50.0,
+          color: Colors.black54,
+        ),
+      ),
+      Positioned.fill(
+        child: GestureDetector(
+          onTap: () {
+            print("add vault tapped");
+            //addToGrid();
+          },
+          child: const Icon(
+            Icons.add_box,
+            size: 100,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    ],
+  ),
+  Container(
+    margin: const EdgeInsets.all(10.0),
+    width: 30.0,
+    height: 50.0,
+    color: Colors.yellow,
+  ),
+];
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+class _HomeScreenState extends State<HomeScreen> {
+  void addToGrid() {
     setState(() {
-      _counter++;
+      gridChild.add(Container(
+        margin: const EdgeInsets.all(8.0),
+        width: 30.0,
+        height: 50.0,
+        color: Colors.purple,
+      ));
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
+    return Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: GestureDetector(
+            onTap: () {
+              print("core vault tapped -> this goes to core vault page");
+            },
+            child: const SizedBox(
+              width: 500,
+              height: 150,
+              child: DecoratedBox(
+                decoration: BoxDecoration(color: Colors.pink),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.blue,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.lock),
-            label: 'Bank',
+        Expanded(
+          child: GridView.count(
+            crossAxisCount: 2,
+            children:
+                List.generate(gridChild.length, (index) => gridChild[index]),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.extension),
-            label: 'Game',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_box),
-            label: 'Add Word',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.group),
-            label: 'Friends',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Account',
-          ),
-        ],
-        selectedItemColor: Colors.white,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        )
+      ],
+    ));
   }
 }
