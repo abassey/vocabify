de50f60d14be98bd98d3df90a5467a9eb534a278
James Lemieux <jimmylemieux9@gmail.com>
2022-02-26 16:13:58 -0500

    reverted Home Widget

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const VaultView(vaultTitle: "Vault Title")
+      home: const MyHomePage(title: "test")
     );
   }
 }
