4cd5ea761fe7d181e32974ce2123284f9413ebe8
James Lemieux <jimmylemieux9@gmail.com>
2022-02-26 16:14:48 -0500

    home page fix

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: "test")
+      home: const MyHomePage(title: 'Vocabify')
     );
   }
 }
